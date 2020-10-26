0a2fe6e619ded78c141f98408d6318554c5edb44
Mohsen Galal <devmohsengalal@gmail.com>
2020-10-26 23:43:27 +0200

    LTR Version

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,12 +1,24 @@
 $(document).ready(function () {
   $(".custom-select").select2();
   $('[data-toggle="tooltip"]').tooltip();
-  $('.main-slider .owl-carousel,.news-slider .owl-carousel').owlCarousel({
-    nav:false,
-    items:1,
-    dots:true,
-    rtl:true
-})
+  isRtl=false;
+  var maxmizeText="Maxmize the list";
+  var minimizeText="Minimize the list"
+  if($("html").attr("dir")=="rtl"){
+    isRtl=true;
+    maxmizeText="تكبير القائمة";
+    minimizeText="تصغير القائمة"
+  }
+  var  $owl;
+  if($('.main-slider .owl-carousel,.news-slider .owl-carousel').length){
+  $owl= $('.main-slider .owl-carousel,.news-slider .owl-carousel').owlCarousel({
+      nav:false,
+      items:1,
+      dots:true,
+      rtl:isRtl
+  })
+  }
+
   // var mainSwiper = new Swiper(".main-slider", {
   //   loop: true,
 
@@ -29,17 +41,17 @@ $(document).ready(function () {
     $(".user-info-card").toggleClass("collapsed");
     $("#user-info-other-side").toggleClass("collapsed");
     if ($(".user-info-card").hasClass("collapsed")) {
-      $(this).attr("data-original-title", "تكبير القائمة");
+      $(this).attr("data-original-title", maxmizeText);
     } else {
-      $(this).attr("data-original-title", "تصغير القائمة");
+      $(this).attr("data-original-title", minimizeText);
     }
     $(this).blur();
-    // var interval = setInterval(function () {
-    //   mainSwiper.update();
-    // }, 5);
-    // setTimeout(function () {
-    //   clearInterval(interval);
-    // }, 300);
+    var interval = setInterval(function () {
+      $owl.trigger('refresh.owl.carousel');
+    }, 5);
+    setTimeout(function () {
+      clearInterval(interval);
+    }, 300);
   });
   $("#fav-toggle").on("click", function () {
     $("#fav-list").slideToggle();
